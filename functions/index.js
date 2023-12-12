48a50bcb6deebe01426bb7217ef30cf9e742d26e
Philip D'Aloia <philip.daloia@gmail.com>
2023-12-11 23:32:07 -0500

    Creating cloud function to listen to changes on the user document and determine if an invite has been added. Need to implement send notification code now.

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,22 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require("firebase-functions");
+const {log} = require("firebase-functions/logger");
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+exports.sendInviteNotification = functions.firestore
+    .document("users/{docId}")
+    .onUpdate((change, context) => {
+      log("Starting function 3");
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+      const newValue = change.after.data();
+      const oldValue = change.before.data();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+      const oldInvites = oldValue.invitedCottageIDs;
+      const newInvites = newValue.invitedCottageIDs;
+      log(oldInvites);
+      log(newInvites);
+
+      if (newInvites.length > oldInvites.length) {
+        log("Invite added, should send notification");
+      }
+
+      log("Ending function 3");
+    });
